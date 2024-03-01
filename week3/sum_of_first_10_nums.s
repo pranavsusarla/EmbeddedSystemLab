97f22dd0744ac230742843ae523a16e829d8736c
randomcuboid06 <112553686+randomcuboid06@users.noreply.github.com>
2024-03-01 11:54:44 +0530

    Create sum_of_first_10_nums.s

---
+++ week3/sum_of_first_10_nums.s
@@ -0,0 +1,20 @@
+		AREA sum,CODE,READONLY
+		ENTRY
+		EXPORT __main
+
+__main
+		MOV R0,#1;
+loop
+		ADD R1,R0;
+		ADD R0,#1;
+		ADD R3, R1, #0;
+		CMP R0,#11
+		BNE loop; BNE -> Branch Not Equal
+		LDR R2,=Result;
+		STR R1,[R2];
+STOP B STOP
+		AREA MulRes,DATA,READWRITE
+Result DCD 0X00;
+		END
+			
+; DONT USE ANY SPACES LIKE MUL R1, R2; do it like this MUL R1,R2;
